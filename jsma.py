e7f65bbde78076eced394f5737865246b9257954
shrrynsh <shrynshmodi@gmail.com>
2026-03-10 15:38:28 +0000

    add saliency_map_increase and saliency_map_decrease functions to jsma.py

--- jsma.py
+++ jsma.py
@@ -33,6 +33,86 @@ def compute_jacobian(
     return jacobian
 
 
+def saliency_map_increase(
+    jacobian :torch.Tensor,
+    target : int,
+    search_domain : set,
+    num_features : int,
+) -> Tuple[int,int];
+
+    num_classes=jacobian.shape[0]
+
+    target_grad=jacobian[target]
+
+    other_grad=jcobian.sum(dim=0)-target_grad
+
+    domain=sorted(search_domain)
+    n=len(domain)
+
+    best_score=-1.0
+    best_p1,best_p2=-1,-1
+
+    for idx_i in range(n):
+        p=domain[idx_i]
+        for idx_j in range(idx_i+1,n):
+            q=domain[idx_j]
+
+            alpha=target_grad[p]+target_grad[q]
+            beta=other_grad[p]+other_grad[q]
+
+            if alpha >0 and beta < 0:
+                score= alpha*(-beta)
+                if score>bet_score:
+                    best_score=score
+                    best_p1,best_p2=p,q
+                    
+    return best_p1,best_p2
 
 
+
+def saliency_map_decrease(
+    jacobian :torch.Tensor,
+    target: int,
+    search_domain:set,
+    num_features:int,
+) -> Tuple[int,int]:
+
+    num_classes=jacobian.shape[0]
+
+    target_grad=jacobian[target]
+
+    other_grad=jcobian.sum(dim=0)-target_grad
+
+    domain=sorted(search_domain)
+    n=len(domain)
+
+    best_score=-1.0
+    best_p1,best_p2=-1,-1
+
+    for idx_i in range(n):
+        p=domain[idx_i]
+        for idx_j in range(idx_i+1,n):
+            q=domain[idx_j]
+
+            alpha=target_grad[p]+target_grad[q]
+            beta=other_grad[p]+other_grad[q]
+
+            if alpha <0 and beta >0 0:
+                score= -alpha*(beta)
+                if score>best_score:
+                    best_score=score
+                    best_p1,best_p2=p,q
+
+    return best_p1,best_p2
+
+
+def jsma_attack(
+    model : nn.Module,
+    x: torch.Tensor,
+    target_class: int,
+    thetha : float,
+    max_distortion: float = 0.145
+    increase : bool=True,
+    
+)
 
